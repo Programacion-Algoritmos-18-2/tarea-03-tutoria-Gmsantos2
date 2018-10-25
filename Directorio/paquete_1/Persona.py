ca976fd4005c7ac7258c478e9d03a3bb68ffd915
Gerson Santos <gersantos1234@gmail.com>
2018-10-24 21:57:10 -0500

    Agregando ehercicios tutoria 03

--- Directorio/paquete_1/Persona.py
+++ Directorio/paquete_1/Persona.py
@@ -1,35 +1,34 @@
-class Docente:
-	def __init__(self,n,a):
+class Docente: #Clase docente
+	def __init__(self,n,a): # se define el __init__  con las variables a utilizar
 		self.nombre = n
 		self.ciudad = a
-	def setnombre(self,n):
+	def setnombre(self,n): # agregamos la variable nombre
 		self.nombre = n
-	def setciudad(self,a):
+	def setciudad(self,a): #agregamos la variable ciudad
 		self.ciudad = a
-	def getnombre(self):
+	def getnombre(self): #obtener nombre (retorna el valor)
 		return self.nombre
-	def getciudad(self):
+	def getciudad(self): # obtener ciudad (retorna el valor)
 		return self.ciudad
-	def presentarD (self):
+	def presentarD (self): #funcion presentar datos
 		cadena = "%s\n\t%s" % (self.nombre,self.ciudad)
 
-class Estudiante:
+class Estudiante: #Clase estudiante
 
-	def __init__(self,  n, lista_docentes):
+	def __init__(self,  n, lista_docentes): #se define el __init__  con las variables a utilizar
 		self.nombres = n
 		self.docentes = lista_docentes
-	def setnombres(self, n):
+	def setnombres(self, n): # agregar nombres
 		self.nombres = n
-	def setdocentes(self,lista_docentes):
-		for i in (len(lista_docentes)):
+	def setdocentes(self,lista_docentes):# agregamos docentes (lista)
 			self.docentes = lista_docentes
-	def getnombres(self):
+	def getnombres(self): #obtener nombres
 		return self.nombres
-	def getdocentes(self):
+	def getdocentes(self): #obtener docentes
 		return self.docentes
-	def presentarD (self):
-		cadena = "Estudiante : %s\n" % (self.getnombres())
+	def presentarD (self): #presentamos 
+		cadena = "Estudiante : %s\n" % (self.getnombres()) #cadena donde nos presenta el nombre del estudiante
 		cadena = "%s%s\n" % (cadena,"Lista de docentes:")
 		for i in range (len(self.docentes)):
-			cadena = "%s\n%s---%s" % (cadena,self.docentes[i].getnombre(),self.docentes[i].getciudad())
+			cadena = "%s\n%s---%s" % (cadena,self.docentes[i].getnombre(),self.docentes[i].getciudad()) #cadena donde nos presenta el nombre y cuidad de cada docente
 		return cadena
--- Directorio/principal.py
+++ Directorio/principal.py
@@ -1,7 +1,7 @@
-from paquete_1.Persona import *
+from paquete_1.Persona import * #importamos el paquete 
 
-d = Docente("Docente B D","Loja")
+d = Docente("Docente B D","Loja") #variable con los datos  para docente
 d2 = Docente("Docente Pray","Quito")
-listado = [d,d2]
-e = Estudiante("Luis",listado)
-print(e.presentarD())
+listado = [d,d2] #esas variables  metemos en una lista 
+e = Estudiante("Luis",listado) #llamamos al metodo estudiante con  los datos "luis" y la lista
+print(e.presentarD()) #presentamos de  la clase estudiante
